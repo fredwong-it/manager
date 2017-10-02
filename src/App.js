aee925cfb5f8776b28e6733b0642f36ca0d590d8
Fred Wong <fredwong.it@gmail.com>
2017-10-02 10:20:57 -0400

    Setup firebase configuration in componentWillMount()

--- src/App.js
+++ src/App.js
@@ -2,9 +2,23 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyCBL0rT1-7G0JM6C2zQdCrkUDMo7DFvVZ8',
+      authDomain: 'anager-c3361.firebaseapp.com',
+      databaseURL: 'https://manager-c3361.firebaseio.com',
+      projectId: 'manager-c3361',
+      storageBucket: '',
+      messagingSenderId: '346056710361'
+    };
+
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
